698c74478aa78c0254023de2204a4077a24684b7
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-12 22:45:19 +0530

    [Adding api logic to get all users from db]

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,20 +1,30 @@
 import User from '../model/userSchema.js';
 
+export const getAllUsers = async (request, response) => {
+    try {
+        const users = await User.find({});
+
+        response.status(200).json(users);
+    } catch (error) {
+        response.json('Error: ', error.message);
+    }
+}
+
 export const userLogIn = async (request, response) => {
     try {
         const uname = request.body.username;
         const pwd = request.body.password;
         let user = await User.findOne({ username: uname, password: pwd });
         if (user) {
-            return response.status(200).json({ data : user});
+            return response.status(200).json({ data: user });
         } else {
             let userEmailObj = await User.findOne({ email: uname, password: pwd });
             if (userEmailObj) {
-                return response.status(200).json({ data : userEmailObj});
+                return response.status(200).json({ data: userEmailObj });
             } else {
                 let userPhoneObj = await User.findOne({ phone: uname, password: pwd });
                 if (userPhoneObj) {
-                    return response.status(200).json({ data : userPhoneObj});
+                    return response.status(200).json({ data: userPhoneObj });
                 } else {
                     return response.status(401).json('Invalid Login');
                 }
--- routes/route.js
+++ routes/route.js
@@ -1,13 +1,14 @@
 import express from  'express';
 
 import { getProductById, getProducts } from '../controller/product-controller.js';
-import { userSignUp, userLogIn } from '../controller/user-controller.js';
+import { userSignUp, userLogIn, getAllUsers } from '../controller/user-controller.js';
 
 const router = express.Router();
 
 //login & signup
 router.post('/signup', userSignUp);
 router.post('/login', userLogIn);
+router.get('/users', getAllUsers);
 
 router.get('/products', getProducts);
 router.get('/product/:id', getProductById);
