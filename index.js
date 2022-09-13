4f122c3cc28af86dccd2289e61e34563fc35ec74
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-13 22:50:22 +0530

    [Updating index.js to export env variables]

--- index.js
+++ index.js
@@ -44,6 +44,15 @@ paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
 paytmParams['ORDER_ID'] = uuid(),
 paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
 paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = 'http://localhost:8000/callback'
-paytmParams['EMAIL'] = 'codeforinterview01@gmail.com'
+paytmParams['CALLBACK_URL'] = process.env.PAYTM_CALLBACK_URL,
+paytmParams['EMAIL'] = process.env.MY_EMAIL,
 paytmParams['MOBILE_NO'] = '1234567852'
+
+export let PAYTM_MID=process.env.PAYTM_MID;
+export let PAYTM_WEBSITE=process.env.PAYTM_WEBSITE;
+export let PAYTM_CHANNEL_ID=process.env.PAYTM_CHANNEL_ID;
+export let PAYTM_INDUSTRY_TYPE_ID=process.env.PAYTM_INDUSTRY_TYPE_ID;
+export let PAYTM_CUST_ID=process.env.PAYTM_CUST_ID;
+export let PAYTM_MERCHANT_KEY=process.env.PAYTM_MERCHANT_KEY;
+export let PAYTM_CALLBACK_URL=process.env.PAYTM_CALLBACK_URL;
+export let MY_EMAIL=process.env.MY_EMAIL;
