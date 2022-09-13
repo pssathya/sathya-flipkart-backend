4b426ccd368578db4405f8b512aa27b069faf4f1
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-14 05:12:36 +0530

    [removing unnecessary imports and exports from index.js]

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
-import { v4 as uuid } from 'uuid';
 
 import Connection from './database/db.js';
 import InsertDefaultData from './insertProductsToMongoDb.js';
@@ -35,23 +34,9 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 app.use('/', Routes);
 
-export let paytmParams = {};
-paytmParams['MID'] = process.env.PAYTM_MID,
-paytmParams['WEBSITE'] = process.env.PAYTM_WEBSITE,
-paytmParams['CHANNEL_ID'] = process.env.PAYTM_CHANNEL_ID,
-paytmParams['INDUSTRY_TYPE_ID'] = process.env.PAYTM_INDUSTRY_TYPE_ID,
-paytmParams['ORDER_ID'] = uuid(),
-paytmParams['CUST_ID'] = process.env.PAYTM_CUST_ID,
-paytmParams['TXN_AMOUNT'] = '100',
-paytmParams['CALLBACK_URL'] = process.env.PAYTM_CALLBACK_URL,
-paytmParams['EMAIL'] = process.env.MY_EMAIL,
-paytmParams['MOBILE_NO'] = '1234567852'
-
-export let PAYTM_MID=process.env.PAYTM_MID;
-export let PAYTM_MERCHANT_KEY=process.env.PAYTM_MERCHANT_KEY;
-export let PAYTM_WEBSITE=process.env.PAYTM_WEBSITE;
-export let PAYTM_CHANNEL_ID=process.env.PAYTM_CHANNEL_ID;
-export let PAYTM_INDUSTRY_TYPE_ID=process.env.PAYTM_INDUSTRY_TYPE_ID;
-export let PAYTM_CUST_ID=process.env.PAYTM_CUST_ID;
-export let PAYTM_CALLBACK_URL=process.env.PAYTM_CALLBACK_URL;
-export let MY_EMAIL=process.env.MY_EMAIL;
+export let PAYTM_MID = process.env.PAYTM_MID;
+export let PAYTM_MERCHANT_KEY = process.env.PAYTM_MERCHANT_KEY;
+export let PAYTM_WEBSITE = process.env.PAYTM_WEBSITE;
+export let PAYTM_CHANNEL_ID = process.env.PAYTM_CHANNEL_ID;
+export let PAYTM_INDUSTRY_TYPE_ID = process.env.PAYTM_INDUSTRY_TYPE_ID;
+export let PAYTM_CALLBACK_URL = process.env.PAYTM_CALLBACK_URL;
