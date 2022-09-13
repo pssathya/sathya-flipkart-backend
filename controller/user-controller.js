0202b61934dcfe596ba33ce89bc7e1f597db9a38
ps_sathya@yahoo.com <ps_sathya@yahoo.com>
2022-09-13 08:55:40 +0530

    [Adding jwt logic for signup & login]

--- controller/user-controller.js
+++ controller/user-controller.js
@@ -1,6 +1,8 @@
+import jwt from 'jsonwebtoken';
 import bcrypt from 'bcryptjs';
 
 import User from '../model/userSchema.js';
+import SuperSecret from '../config.js';
 
 export const getAllUsers = async (request, response) => {
     try {
@@ -8,39 +10,47 @@ export const getAllUsers = async (request, response) => {
 
         response.status(200).json(users);
     } catch (error) {
-        response.json('Error: ', error.message);
+        response.status(500).json('Error: ', error.message);
     }
+}
+
+async function getLoginUser(uname) {
+    let user = await User.findOne({ username: uname });
+    if (!user) {
+        user = await User.findOne({ email: uname });
+        if (!user) {
+            user = await User.findOne({ phone: uname });
+        }
+    }
+    return user;
 }
 
 export const userLogIn = async (request, response) => {
     try {
         const uname = request.body.username;
         const pwd = request.body.password;
-        let user = await User.findOne({ username: uname, password: pwd });
+        let user = await getLoginUser(uname);
         if (user) {
-            return response.status(200).json({ data: user });
-        } else {
-            let userEmailObj = await User.findOne({ email: uname, password: pwd });
-            if (userEmailObj) {
-                return response.status(200).json({ data: userEmailObj });
-            } else {
-                let userPhoneObj = await User.findOne({ phone: uname, password: pwd });
-                if (userPhoneObj) {
-                    return response.status(200).json({ data: userPhoneObj });
-                } else {
-                    return response.status(401).json('Invalid Login');
-                }
-            }
+            const isPassValid = bcrypt.compareSync(request.body.password, user.password);
+            if (!isPassValid)
+                return response.status(401).json({ auth: false, token: 'Invalid Password' });
+            // in case both match 
+            let token = jwt.sign({ id: user._id }, SuperSecret.secret, { expiresIn: 86400 }); //24 hours
+            return response.status(200).json({ auth: true, userFirstName: user.firstname, token: token });
         }
-
+        else {
+            return response.status(401).json({ auth: false, token: 'Invalid Password' });
+        }
     } catch (error) {
-        response.json('Error: ', error.message);
+        response.status(500).json('Error: ', error.message);
     }
 }
 
 export const userSignUp = async (request, response) => {
     try {
-        const exist = await User.findOne({ username: request.body.username });
+        const uname = request.body.username;
+
+        const exist = await User.findOne({ username: uname });
         if (exist) {
             return response.status(401).json({ message: 'User already exist' });
         }
@@ -51,7 +61,10 @@ export const userSignUp = async (request, response) => {
         user.password = hashPassword;
         const newUser = new User(user);
         await newUser.save();
-        response.status(200).json({ mesage: user });
+
+        const userObj = await User.findOne({ username: uname });
+        let token = jwt.sign({ id: userObj._id }, SuperSecret.secret, { expiresIn: 86400 }); //24 hours
+        return response.status(200).json({ auth: true, userFirstName: userObj.firstname, token: token });
 
     } catch (error) {
         response.status(500).json({ message: error.message });
